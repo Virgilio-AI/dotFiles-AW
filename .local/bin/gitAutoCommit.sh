6db1b95434a93c56371d0835ce5f1094e66ca133
Virgilio Murillo <virgiliomurilloochoa1@gmail.com>
2021-09-05 17:00:34 -0500

    sync some files for gitAutoCommit

--- .local/bin/gitAutoCommit.sh
+++ .local/bin/gitAutoCommit.sh
@@ -2,13 +2,12 @@
 #  AutoCommit multiple repos
 #
 
-sudo rsync -aAXv --delete ~/.config/ ~/Documents/GitRepos/dotFiles-AW/.config/
-sudo rsync -aAXv --delete ~/.local/ ~/Documents/GitRepos/dotFiles-AW/.local/
-sudo rsync -aAXv --delete ~/images/ ~/Documents/GitRepos/dotFiles-AW/images/
-sudo rsync -AXv ~/Documents/GitRepos/installationGuide-AW/main.pdf ~/Documents/GitRepos/archiso-AW/airootfs/root/InstallationGuide.pdf
+sudo rsync -aAXv --delete --exclude=.git ~/.config/ ~/Documents/GitRepos/dotFiles-AW/.config/
+sudo rsync -aAXv --delete --exclude=.git ~/.local/ ~/Documents/GitRepos/dotFiles-AW/.local/
+sudo rsync -aAXv --delete --exclude=.git ~/images/ ~/Documents/GitRepos/dotFiles-AW/images/
+sudo rsync -AXv --exclude=.git ~/Documents/GitRepos/installationGuide-AW/main.pdf ~/Documents/GitRepos/archiso-AW/airootfs/root/InstallationGuide.pdf
 
 # sync all files first
-rsync
 cd $HOME/Documents/GitRepos
 for f in *; do
 	if [ -d "$f" ]; then
