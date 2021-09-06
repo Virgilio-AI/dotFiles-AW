d2f898c6c83847e8e40b52868b243829190478ce
Virgilio Murillo <virgiliomurilloochoa1@gmail.com>
2021-09-06 17:26:10 -0500

    changed gitAutoCommit so that zsh files get synqued before the script execute

--- .local/bin/gitAutoCommit.sh
+++ .local/bin/gitAutoCommit.sh
@@ -7,6 +7,22 @@ sudo rsync -aAXv --delete --exclude=.git ~/.local/ ~/Documents/GitRepos/dotFiles
 sudo rsync -aAXv --delete --exclude=.git ~/images/ ~/Documents/GitRepos/dotFiles-AW/images/
 sudo rsync -AXv --exclude=.git ~/Documents/GitRepos/installationGuide-AW/main.pdf ~/Documents/GitRepos/archiso-AW/airootfs/root/InstallationGuide.pdf
 
+# for zsh files and profile file
+
+
+sudo rsync -AXv /etc/profile ~/Documents/GitRepos/archiso-AW/airootfs/root/etc/profile
+sudo rsync -AXv /etc/profile ~/Documents/GitRepos/autoInstaller-AW/profile
+
+# work to do after you have users
+# sudo rsync -AXv $ZDOTDIR/.zshrc ~/Documents/GitRepos/archiso-AW/airootfs/root/etc/zsh/zshrc
+sudo rsync -AXv $ZDOTDIR/  ~/Documents/GitRepos/autoInstaller-AW/zdotdir/
+sudo rsync -AXv /etc/zsh/  ~/Documents/GitRepos/autoInstaller-AW/etcZdotdir/
+
+
+
+# tasks = check if all folders exists, if not create them
+# \ modify the autoinstaller so that all of the files get installed by default
+
 # sync all files first
 cd $HOME/Documents/GitRepos
 for f in *; do
