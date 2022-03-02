591279a9e8cdc942055f6c724819f54746385157
Virgilio Murillo <virgiliomurilloochoa1@gmail.com>
2022-03-01 19:38:26 -0600

    ignored the qutebrowser files to sync

--- .local/bin/utilities/gitAutoCommit.zsh
+++ .local/bin/utilities/gitAutoCommit.zsh
@@ -24,7 +24,7 @@ sudo rsync -aAXv --delete --exclude=.git ~/.local/src/st/ ~/Documents/GitRepos/A
 # ==========================
 
 sudo rsync -aAXv --delete --exclude={'.git','BraveSoftware','cfiles','Electron','google-chrome','libreoffice','qutebrowser','Udeler','GeoGebra','GIMP','inkscape','unity3d','unityhub','UnityHub','FreeCAD'} ~/.config/ ~/Documents/GitRepos/dotFiles-AW/.config/ ;
-sudo rsync -aAXv --delete --exclude={'.git','Proteus 7 Professional'} ~/.local/ ~/Documents/GitRepos/dotFiles-AW/.local/ ;
+sudo rsync -aAXv --delete --exclude={'qutebrowser','.git','Proteus 7 Professional'} ~/.local/ ~/Documents/GitRepos/dotFiles-AW/.local/ ;
 sudo rsync -aAXv --delete --exclude=.git ~/images/ ~/Documents/GitRepos/dotFiles-AW/images/ ;
 sudo rsync -aAXv --exclude=.git ~/Documents/GitRepos/installationGuide-AW/main.pdf ~/Documents/GitRepos/archiso-AW/airootfs/root/InstallationGuide.pdf ;
 
